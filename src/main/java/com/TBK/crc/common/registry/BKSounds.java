94e43ec596052131e8c30ab807de2f5b9c2f8373
SangreBK <SangreBK@gmail.com>
2025-10-14 14:27:56 -0300

    update

---
+++ src/main/java/com/TBK/crc/common/registry/BKSounds.java
@@ -0,0 +1,62 @@
+package com.TBK.crc.common.registry;
+
+
+import com.TBK.crc.CRC;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class BKSounds {
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, CRC.MODID);
+
+
+    //ENTITY
+
+    public static final RegistryObject<SoundEvent> CHICKEN_BOMB_LOOP =
+            registerSoundEvent("chicken_bomb_loop");
+
+    public static final RegistryObject<SoundEvent> CHICKEN_MINION_HURT =
+            registerSoundEvent("chicken_minion_hurt");
+
+    public static final RegistryObject<SoundEvent> REX_LASER_LOOP =
+            registerSoundEvent("rex_laser_loop");
+
+    public static final RegistryObject<SoundEvent> REX_LASER_SHOOT =
+            registerSoundEvent("rex_laser_shoot");
+
+    public static final RegistryObject<SoundEvent> REX_START_CHARGE =
+            registerSoundEvent("rex_start_charge");
+
+    public static final RegistryObject<SoundEvent> REX_STEP1 =
+            registerSoundEvent("rex_step1");
+
+    public static final RegistryObject<SoundEvent> REX_STEP2 =
+            registerSoundEvent("rex_step2");
+
+    public static final RegistryObject<SoundEvent> REX_STOMP =
+            registerSoundEvent("rex_stomp");
+
+    //CYBER-IMPLANTS
+
+    public static final RegistryObject<SoundEvent> HEART_REFLEX =
+            registerSoundEvent("heart_reflex");
+
+    //BLOCKS
+
+    public static final RegistryObject<SoundEvent> CYBORG_TABLE_IMPLANT =
+            registerSoundEvent("heart_reflex");
+
+
+
+    public static RegistryObject<SoundEvent> registerSoundEvent(String name){
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(CRC.MODID, name)));
+    }
+
+    public static void register(IEventBus eventBus){
+        SOUND_EVENTS.register(eventBus);
+    }
+}
